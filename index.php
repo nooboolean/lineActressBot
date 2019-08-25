887d638acae6578d9c9988643d6dcabe9a946051
Yoshihiro <b1014225@fun.ac.jp>
2019-08-25 21:10:41 +0900

    ソースコードテスト

--- index.php
+++ index.php
@@ -1,35 +1,31 @@
 <?php
-DEFINE("ACCESS_TOKEN","dQLkvDlb84bZwwxGovklsbNSizODCximzaLmOrYuVZ+nPPBn0YdrAFDEAdpu/QJNjrdUC72OTqXN7EaCilQQBidTfp+5JeJSXVGv19AaUeD60eGH/fPV8J5Mnkh3QE2dv2C68xKyfDRZe/0hZgaUrwdB04t89/1O/w1cDnyilFU=");
-DEFINE("SECRET_TOKEN","c80619da86ed696ddc27a0876af8b2d0");
+require_once __DIR__ . '/vendor/autoload.php';
+//POST
+$input = file_get_contents('php://input');
+$json = json_decode($input);
+$event = $json->events[0];
 
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot;
-use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
-use \LINE\LINEBot\Constant\HTTPHeader;
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(CHANNEL_ACCESS_TOKEN);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => CHANNEL_ACCESS_TOKEN]);
 
-//LINESDKの読み込み
-require_once(__DIR__."/vendor/autoload.php");
-
-//LINEから送られてきたらtrueになる
-if(isset($_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE])){
-
-//LINEBOTにPOSTで送られてきた生データの取得
-  $inputData = file_get_contents("php://input");
-
-//LINEBOTSDKの設定
-  $httpClient = new CurlHTTPClient(ACCESS_TOKEN);
-  $bot = new LINEBot($HttpClient, ['channelSecret' => SECRET_TOKEN]);
-  $signature = $_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE]; 
-  $Events = $bot->parseEventRequest($inputData, $signature);
-
-//大量にメッセージが送られると複数分のデータが同時に送られてくるため、foreachをしている。
-  foreach($Events as $event){
-    $SendMessage = new MultiMessageBuilder();
-    $TextMessageBuilder = new TextMessageBuilder("よろぽん！");
-    $SendMessage->add($TextMessageBuilder);
-    $bot->replyMessage($event->getReplyToken(), $SendMessage);
-  }
-  echo '成功';
+//イベントタイプ判別
+if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
+    //テキストメッセージにはオウムで返す
+    if ("text" == $event->message->type) {
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+    } else {
+        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ごめん、わかんなーい(*´ω｀*)");
+    }
+} elseif ("follow" == $event->type) {        //お友達追加時
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("よろしくー");
+} elseif ("join" == $event->type) {           //グループに入ったときのイベント
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('こんにちは よろしくー');
+} elseif ('beacon' == $event->type) {         //Beaconイベント
+    $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Godanがいんしたお(・∀・) ');
+} else {
+    //なにもしない
 }
-echo '失敗';
+$response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
+syslog(LOG_EMERG, print_r($event->replyToken, true));
+syslog(LOG_EMERG, print_r($response, true));
+return;
