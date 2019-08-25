b221bd96ec38bcbb349355187dfb3197827035bd
Yoshihiro <b1014225@fun.ac.jp>
2019-08-25 20:20:26 +0900

    ソースコード変えてみた

--- index.php
+++ index.php
@@ -1,22 +1,35 @@
 <?php
-    // Composerでインストールしたライブラリを一括読み込み
-    require_once __DIR__ . '/vendor/autoload.php';
+DEFINE("ACCESS_TOKEN","dQLkvDlb84bZwwxGovklsbNSizODCximzaLmOrYuVZ+nPPBn0YdrAFDEAdpu/QJNjrdUC72OTqXN7EaCilQQBidTfp+5JeJSXVGv19AaUeD60eGH/fPV8J5Mnkh3QE2dv2C68xKyfDRZe/0hZgaUrwdB04t89/1O/w1cDnyilFU=");
+DEFINE("SECRET_TOKEN","c80619da86ed696ddc27a0876af8b2d0");
 
-    // アクセストークンを使いCurlHTTPClientをインスタンス化
-    $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('Hwb00gSaNTRwf0a81xZiigOQfMc8MEZGB26eGjQ5kw9b9iDOdmVjMH8TTBbt2WSQjrdUC72OTqXN7EaCilQQBidTfp+5JeJSXVGv19AaUeChDlHxBXzEuxsnnpZVtgRbCijOKPiCLRgWcHuH6NNFiwdB04t89/1O/w1cDnyilFU=');
+use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use \LINE\LINEBot;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use \LINE\LINEBot\Constant\HTTPHeader;
 
-    //CurlHTTPClientとシークレットを使いLINEBotをインスタンス化
-    $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '6fd46cbd83f2d4a7ceb364085447663a']);
+//LINESDKの読み込み
+require_once(__DIR__."/vendor/autoload.php");
 
-    // LINE Messaging APIがリクエストに付与した署名を取得
-    $signature = $_SERVER["HTTP_" . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
+//LINEから送られてきたらtrueになる
+if(isset($_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE])){
 
-    //署名をチェックし、正当であればリクエストをパースし配列へ、不正であれば例外処理
-    $events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
+//LINEBOTにPOSTで送られてきた生データの取得
+  $inputData = file_get_contents("php://input");
 
-    foreach ($events as $event) {
-        // メッセージを返信
-        $response = $bot->replyMessage(
-            $event->getReplyToken(), new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->getText())  
-        );
-    }
+//LINEBOTSDKの設定
+  $httpClient = new CurlHTTPClient(ACCESS_TOKEN);
+  $bot = new LINEBot($HttpClient, ['channelSecret' => SECRET_TOKEN]);
+  $signature = $_SERVER["HTTP_".HTTPHeader::LINE_SIGNATURE]; 
+  $Events = $bot->parseEventRequest($inputData, $signature);
+
+//大量にメッセージが送られると複数分のデータが同時に送られてくるため、foreachをしている。
+  foreach($Events as $event){
+    $SendMessage = new MultiMessageBuilder();
+    $TextMessageBuilder = new TextMessageBuilder("よろぽん！");
+    $SendMessage->add($TextMessageBuilder);
+    $bot->replyMessage($event->getReplyToken(), $SendMessage);
+  }
+  echo '成功';
+}
+echo '失敗';
