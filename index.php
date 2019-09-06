81f9caf6906e081a719a42dcc35e239c3dbbeaa9
Yoshihiro <b1014225@fun.ac.jp>
2019-09-07 02:54:01 +0900

    wiki api 仕込んでみた

--- index.php
+++ index.php
@@ -12,7 +12,16 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 if ("message" == $event->type) {            //一般的なメッセージ(文字・イメージ・音声・位置情報・スタンプ含む)
     //テキストメッセージにはオウムで返す
     if ("text" == $event->message->type) {
-        $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
+      $url='http://ja.wikipedia.org/w/api.php?format=xml&action=query&list=search&srlimit=50&srsearch=';
+      $url.=urlencode('親の文字体系');
+      $ch      = curl_init($url);
+      curl_setopt_array($ch, $option);
+      $json    = curl_exec($ch);
+      $info    = curl_getinfo($ch);
+      $errorNo = curl_errno($ch);
+      echo 'jsonだあよ！';
+      echo $json;
+      $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($event->message->text);
     } else {
         $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder("ごめん、わかんなーい(*´ω｀*)");
     }
