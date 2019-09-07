6724c156dbe628b6df9fa79a35729d03e4d3c834
Yoshihiro <b1014225@fun.ac.jp>
2019-09-08 03:46:53 +0900

    wiki test

--- test.php
+++ test.php
@@ -1,24 +1,26 @@
 <?php
 
 $url = 'http://ja.wikipedia.org/w/api.php?'
-.'format=json&'
+.'format=xml&'
 .'action=query&'
 .'prop=extracts&'
 .'rvprop=content&'
 .'rvparse&'
 .'titles='.urlencode('宇垣美里');
 
-$json = file_get_contents($url);
-$arry = json_decode($json);
-$arry = $arry->{"query"}->{"pages"};
-foreach($arry as $arry1){
-  foreach ($arry1 as $key => $value) {
-    if($key == 'extract'){
-      $data = $value;
-      $data = serialize($data);
-      var_dump($data);
-    }
-  }
-}
+$json = file_get_contents("http://wikipedia.simpleapi.net/api?keyword=宇垣美里&output=xml");
+$xml = new SimpleXMLElement($json);
+$xml = $xml->result[0]->body;
+// $arry = json_decode($json);
+// $arry = $arry->{"query"}->{"pages"};
+// foreach($arry as $arry1){
+//   foreach ($arry1 as $key => $value) {
+//     if($key == 'extract'){
+//       $data = $value;
+//       $data = serialize($data);
+//       var_dump($data);
+//     }
+//   }
+// }
 // $data = serialize($data);
-// var_dump($data);
+var_dump($xml);
