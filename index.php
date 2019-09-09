8c2ca97365c788a9eb0d425f1f1070f017b5d3fa
Yoshihiro <b1014225@fun.ac.jp>
2019-09-09 12:39:48 +0900

    reply count の保持がうまくいかない

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ $input       = file_get_contents('php://input');
 $json        = json_decode($input);
 $event       = $json->events[0];
 $reply_count = 0;
+$output = '';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET')]);
@@ -12,17 +13,20 @@ $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => getenv('CHANNEL_SECRET
 if ($event->type == "message") {
   if ($event->message->type == "text") {
     $received_message = $event->message->text;
-    if (preg_match('/変態メガネ/', $received_message)){
-      $output = '調べて欲しい女優の名前を言ってみたまえ';
-    } else {
+    if ($reply_count > 0){
       $xml = file_get_contents("http://wikipedia.simpleapi.net/api?keyword=${received_message}&output=xml");
       $xml = simplexml_load_string($xml);
-      $output = '';
       if ($xml->result[0]->strict == 1){
         $output = $xml->result[0]->body;
         $output = (string)$output;
       } else {
         $output = 'ちょっと何言ってるかわかんないっす(*´ω｀*)';
+      }
+      $reply_count--;
+    } else {
+      if (preg_match('/変態メガネ/', $received_message)){
+        $output = '調べて欲しい女優の名前を言ってみたまえ';
+        $reply_count++;
       }
     }
   }
@@ -31,4 +35,5 @@ $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($outpu
 $response = $bot->replyMessage($event->replyToken, $textMessageBuilder);
 syslog(LOG_EMERG, print_r($event->replyToken, true));
 syslog(LOG_EMERG, print_r($response, true));
+syslog(LOG_EMERG, print_r($reply_count, true));
 return;
